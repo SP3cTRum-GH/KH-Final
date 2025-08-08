7ec39d78ee32ad7ba32068b9c27cc97b8169bc7f
강지현 <wlgus4083@gmail.com>
2025-08-08 10:58:55 +0900

    [update] detailpage 더 보기 버튼 기능 구현

--- front/shopping-mall/src/components/detailpage/ProductImageList.jsx
+++ front/shopping-mall/src/components/detailpage/ProductImageList.jsx
@@ -1,26 +1,52 @@
-import React from "react";
-import { Img, ImgContainer } from "./ProductImageListStyle";
+import React, { useState } from "react";
+import { Img, ImgContainer, MoreButton } from "./ProductImageListStyle";
+
+const imgList = [
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+  {
+    itemImg:
+      "https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg",
+  },
+];
 
 const ProductImageList = () => {
+  const [view, setView] = useState(false);
+  const displayList = view ? imgList : imgList.slice(0, 3);
+
+  const viewClick = () => {
+    setView(true);
+  };
+
   return (
-    <ImgContainer>
-      <Img
-        src="https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg"
-        alt=""
-      />
-      <Img
-        src="https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg"
-        alt=""
-      />
-      <Img
-        src="https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg"
-        alt=""
-      />
-      <Img
-        src="https://cdn.imweb.me/upload/S20200108f8bf70c997599/ac6ba4175217f.jpg"
-        alt=""
-      />
-    </ImgContainer>
+    <>
+      <ImgContainer>
+        {displayList.map((imgs, i) => {
+          return <Img key={i} src={imgs.itemImg} alt="제품 이미지" />;
+        })}
+        {!view && imgList.length > 3 && (
+          <MoreButton onClick={viewClick}>더 보기</MoreButton>
+        )}
+      </ImgContainer>
+    </>
   );
 };
 
